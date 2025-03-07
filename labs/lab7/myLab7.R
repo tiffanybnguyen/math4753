4db04b22a61bd971e8c414b4d705558bc41d29f1
Tiffany Nguyen <tiffanynguyenb4@gmail.com>
2025-03-06 21:19:26 -0600

    initial finish of lab7

---
+++ labs/lab7/myLab7.R
@@ -0,0 +1,11 @@
+library(MATH4753nguy0850)
+library(usethis)
+
+fire <- MATH4753nguy0850::myread("FIREDAM.csv", "/Users/tiffanynguyen/Documents/College/Sophomore/Spring_2025/Applied_Statistical_Methods/MATH4753nguy0850/labs/lab7/")
+
+usethis::use_data(fire, overwrite = TRUE)
+devtools::document()
+data("fire")
+knitr::kable(head(fire))
+
+fire
