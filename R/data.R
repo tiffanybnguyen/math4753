97c01340511cc586374eb87dcd789267f89c9289
Tiffany Nguyen <tiffanynguyenb4@gmail.com>
2025-03-06 21:19:26 -0600

    initial finish of lab7

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Fire data
+#'
+#' A subset of data from somewhere idk man
+#'
+#' @format ## `fire`
+#' A data frame with 15 rows and 2 columns:
+#' \describe{
+#'   \item{DISTANCE}{The Distance}
+#'   \item{DAMAGE}{Amount of Damage}
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"fire"
